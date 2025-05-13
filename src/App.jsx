631641e1505f568adc8ba5b83e0e25fe2a50031b
CarmeloMelito <melitocarmelo211@gmail.com>
2025-05-13 09:34:41 +0200

    feat:add defaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieDetail from "./pages/MovieDetail";
+import DefaultLayout from "./layout/DefaultLayout";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1> home</h1>}></Route>
-        <Route path="/film" element={<Home />}></Route>
-        <Route path="/film/:id" element={<MovieDetail />}></Route>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<h1> home</h1>}></Route>
+          <Route path="/film" element={<Home />}></Route>
+          <Route path="/film/:id" element={<MovieDetail />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
